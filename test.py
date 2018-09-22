f5f482b9ac64203072617191d988a7fdc3bff448
CorinaAlb <cori_alb@yahoo.com>
2018-09-22 18:30:56 +0000

    updated cherrypy

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print 'mergeeee'
+import cherrypy
+
+class HelloWorld(object):
+    @cherrypy.expose
+    def index(self):
+	return "Hallo Welt! ^_^"
+    index.exposed = True
+
+if __name__ == '__main__':
+    cherrypy.config.update({
+    'server.socket_host': '0.0.0.0',
+    'server.socket_port': 8080
+    })
+    cherrypy.quickstart(HelloWorld())
